273ba9eabf794ce984bea708048d61fe7e30b791
Brooklyn P <165840788+broooklynp@users.noreply.github.com>
2025-03-04 18:12:39 -0800

    interaction works

--- radar.js
+++ radar.js
@@ -3,38 +3,44 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 let gamified_data = [];     // Global variable to store the gamified data
 let non_gamified_data = []; // Global variable to store the non-gamified data
 
-// Load the gamified data
-d3.csv("./radar_data/gamified_stats_by_second.csv", (d) => {
+
+async function loadData() {
+  // Load the gamified data
+  gamified_data = await d3.csv("./radar_data/gamified_stats_by_second.csv", (d) => {
     d.time_elapsed = parseFloat(d.time_elapsed);
     d.bvp = parseFloat(d.bvp);
     d.eda = parseFloat(d.eda);
     d.temp = parseFloat(d.temp);
     return d;
-  }).then(gamified_data => {  // Ensure we wait for the CSV data
-    console.log("Loaded Gamified Data:", gamified_data)
   });
+  console.log("Loaded Gamified Data:", gamified_data)
 
-// Load the non-gamified data
-d3.csv("./radar_data/non_gamified_stats_by_second.csv", (d) => {
-  d.time_elapsed = parseFloat(d.time_elapsed);
-  d.bvp = parseFloat(d.bvp);
-  d.eda = parseFloat(d.eda);
-  d.temp = parseFloat(d.temp);
-  return d;
-}).then(non_gamified_data => {  // Ensure we wait for the CSV data
+  // Load the non-gamified data
+  non_gamified_data = await d3.csv("./radar_data/non_gamified_stats_by_second.csv", (d) => {
+    d.time_elapsed = parseFloat(d.time_elapsed);
+    d.bvp = parseFloat(d.bvp);
+    d.eda = parseFloat(d.eda);
+    d.temp = parseFloat(d.temp);
+    return d;
+  });
   console.log("Loaded Non-Gamified Data:", non_gamified_data)
-});
+}
 
-  const width = 500;
-  const height = 500;
+const width = 500;
+const height = 500;
 
-  let svg = d3.select("#radar-chart");
+let svg = d3.select("#radar-chart");
 
-  // Scale for the radius
-  const rScale = d3.scaleLinear()
-      .range([0, 200])
-      .domain([0, 1]);
-  let ticks = [0.2, 0.4, 0.6, 0.8, 1];
+// Scale for the radius
+const rScale = d3.scaleLinear()
+    .range([0, 200])
+    .domain([0, 1]);
+let ticks = [0.2, 0.4, 0.6, 0.8, 1];
+let features = ['bvp', 'eda', 'temp'];
+
+async function drawRadarChart() {
+  // wait for the data to be loaded
+  await loadData();
 	
 	//Draw the background circles
 	svg.selectAll("circle")
@@ -59,7 +65,6 @@ d3.csv("./radar_data/non_gamified_stats_by_second.csv", (d) => {
             .text(d => d.toString())
     );
 
-  let features = ['bvp', 'eda', 'temp'];
   let featureData = features.map((f, i) => {
       let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
       return {
@@ -91,11 +96,126 @@ d3.csv("./radar_data/non_gamified_stats_by_second.csv", (d) => {
               .attr("y", d => d.label_coord.y)
               .text(d => d.name)
       );
+  
+let line = d3.line()
+    .x(d => d.x)
+    .y(d => d.y);
+  
+// draw the path element for non gamified data
+svg.selectAll(".non-gamified-path")
+    .data([non_gamified_data[0]])
+    .join(
+        enter => enter.append("path")
+            .attr("class", "non-gamified-path")
+            .datum(d => getPathCoordinates(d))
+            .attr("d", line)
+            .attr("stroke-width", 3)
+            .attr("stroke", (_, i) => 'gray')
+            .attr("fill", (_, i) => 'gray')
+            .attr("stroke-opacity", 1)
+            .attr("opacity", 0.5)
+    );
 
+// draw the path element for gamified data
+svg.selectAll(".gamified-path")
+    .data([gamified_data[0]])
+    .join(
+        enter => enter.append("path")
+            .attr("class", "gamified-path")
+            .datum(d => getPathCoordinates(d))
+            .attr("d", line)
+            .attr("stroke-width", 3)
+            .attr("stroke", (_, i) => 'darkorange')
+            .attr("fill", (_, i) => 'darkorange')
+            .attr("stroke-opacity", 1)
+            .attr("opacity", 0.5)
+    );
+
+// Add legend
+const legend = svg.append("g")
+.attr("class", "legend")
+.attr("transform", `translate(${width - 130}, 20)`);
+
+legend.append("rect")
+.attr("x", 0)
+.attr("y", 0)
+.attr("width", 20)
+.attr("height", 20)
+.attr("fill", "darkorange")
+.attr("opacity", 0.7);
+
+legend.append("text")
+.attr("x", 30)
+.attr("y", 15)
+.text("Gamified")
+.style("font-size", "14px")
+.attr("alignment-baseline", "middle");
+
+legend.append("rect")
+.attr("x", 0)
+.attr("y", 30)
+.attr("width", 20)
+.attr("height", 20)
+.attr("fill", "gray")
+.attr("opacity", 0.7);
+
+legend.append("text")
+        .attr("x", 30)
+        .attr("y", 45)
+        .text("Non-Gamified")
+        .style("font-size", "14px")
+        .attr("alignment-baseline", "middle");
+
+  document.getElementById("slider").step = "1";
+  const value = document.querySelector("#sec-num");
+  const input = document.querySelector("#slider");
+  value.textContent = input.value;
+  input.addEventListener("input", (event) => {
+        const second = parseInt(event.target.value, 10);
+        value.textContent = second;
+        const gamifiedDataPoint = getDataPointByTimeElapsed(second, gamified_data);
+        const nonGamifiedDataPoint = getDataPointByTimeElapsed(second, non_gamified_data);
+        updateShapes(gamifiedDataPoint, nonGamifiedDataPoint);
+      });
+  
+  // Function to update the shapes with smooth transitions
+  function updateShapes(gamifiedDataPoint, nonGamifiedDataPoint) {
+    svg.selectAll(".gamified-path")
+        .datum(gamifiedDataPoint)
+        .transition()
+        .duration(100)
+        .attr("d", line(getPathCoordinates(gamifiedDataPoint)));
+
+    svg.selectAll(".non-gamified-path")
+        .datum(nonGamifiedDataPoint)
+        .transition()
+        .duration(100)
+        .attr("d", line(getPathCoordinates(nonGamifiedDataPoint)));
+}
+}
+
+drawRadarChart();
+
+// Function to get the data point where time_elapsed equals the slider value
+function getDataPointByTimeElapsed(index, data) {
+  return data[index - 1];
+}
+
+// Function maps polar coordinates to svg coordinates
 function angleToCoordinate(angle, value){
   let x = Math.cos(angle) * rScale(value);
   let y = Math.sin(angle) * rScale(value);
   return {"x": width / 2 + x, "y": height / 2 - y};
+}
+
+function getPathCoordinates(data_point){
+  let coordinates = [];
+  for (var i = 0; i < features.length; i++){
+      let ft_name = features[i];
+      let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+      coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
+  }
+  return coordinates;
 }
 
 // Make the width of the slider the same as the width of the svg
