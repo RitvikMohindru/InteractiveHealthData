120e411b9cc66f25e876d99b9248587c7469a69b
Lasya Yadlapati <syadlapati@ucsd.edu>
2025-03-17 17:20:18 -0700

    pcp plot

--- bvp_variance_line.js
+++ bvp_variance_line.js
@@ -1,256 +1,263 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 async function loadData(file) {
-    let data = await d3.csv(file, (d) => {
-        d.time_elapsed = parseFloat(d.time_elapsed);
-        d.bvp = parseFloat(d.bvp);
-        return d;
-    });
-    return data;
+  let data = await d3.csv(file, (d) => {
+    d.time_elapsed = parseFloat(d.time_elapsed);
+    d.bvp = parseFloat(d.bvp);
+    return d;
+  });
+  return data;
 }
 
 function updateChart(data) {
+  const svgWidth = 800;
+  const svgHeight = 400;
+  const margin = { top: 50, right: 50, bottom: 75, left: 100 };
 
-    const svgWidth = 800;
-    const svgHeight = 400;
-    const margin = { top: 50, right: 50, bottom: 75, left: 100 };
+  const width = svgWidth - margin.left - margin.right;
+  const height = svgHeight - margin.top - margin.bottom;
 
-    const width = svgWidth - margin.left - margin.right;
-    const height = svgHeight - margin.top - margin.bottom;
+  const baselineData = data.filter((d) => d.baseline_cog === "baseline");
+  const cogData = data.filter((d) => d.baseline_cog === "cog");
 
-    const baselineData = data.filter((d) => d.baseline_cog === "baseline");
-    const cogData = data.filter((d) => d.baseline_cog === "cog");
+  const svg = d3.select("#bvp-variance-line-chart");
+  svg.selectAll("*").remove();
 
-    const svg = d3.select("#bvp-variance-line-chart");
-    svg.selectAll("*").remove();
+  const xScale = d3.scaleLinear().domain([0, 120]).range([0, width]);
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.bvp) + 500])
+    .range([300, 0]);
 
-    const xScale = d3.scaleLinear().domain([0, 120]).range([0, width]);
-    const yScale = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.bvp) + 500])
-      .range([300, 0]);
+  const lineBaseline = d3
+    .line()
+    .defined((d) => !isNaN(d.bvp))
+    .x((d) => xScale(d.time_elapsed))
+    .y((d) => yScale(d.bvp));
 
-    const lineBaseline = d3
-      .line()
-      .defined((d) => !isNaN(d.bvp))
-      .x((d) => xScale(d.time_elapsed))
-      .y((d) => yScale(d.bvp));
+  const lineCog = d3
+    .line()
+    .defined((d) => !isNaN(d.bvp))
+    .x((d) => xScale(d.time_elapsed))
+    .y((d) => yScale(d.bvp));
 
-    const lineCog = d3
-      .line()
-      .defined((d) => !isNaN(d.bvp))
-      .x((d) => xScale(d.time_elapsed))
-      .y((d) => yScale(d.bvp));
+  const chartGroup = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    const chartGroup = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);;
+  const cogPath = chartGroup
+    .append("path")
+    .datum(cogData)
+    .attr("d", lineCog)
+    .attr("fill", "none")
+    .attr("stroke", "#FFA500")
+    .attr("stroke-width", 3);
 
-    const cogPath = chartGroup
-      .append("path")
-      .datum(cogData)
-      .attr("d", lineCog)
-      .attr("fill", "none")
-      .attr("stroke", "#FFA500")
-      .attr("stroke-width", 3);
-    
-    const baselinePath = chartGroup
-      .append("path")
-      .datum(baselineData)
-      .attr("d", lineBaseline)
-      .attr("fill", "none")
-      .attr("stroke", "#808080")
-      .attr("stroke-width", 3);
-    
-    const xAxis = d3.axisBottom(xScale).tickSize(7);
-    const yAxis = d3.axisLeft(yScale).tickSize(7);
+  const baselinePath = chartGroup
+    .append("path")
+    .datum(baselineData)
+    .attr("d", lineBaseline)
+    .attr("fill", "none")
+    .attr("stroke", "#808080")
+    .attr("stroke-width", 3);
 
-    chartGroup
-      .append("g")
-      .attr("transform", `translate(0, 300)`)
-      .call(xAxis)
-      .selectAll("text")
-      .style("font-family", "'Merriweather'")
-      .style("font-size", "15px");
-    
-    chartGroup
-      .append("g")
-      .call(yAxis)
-      .selectAll("text")
-      .style("font-family", "'Merriweather'")
-      .style("font-size", "15px");
-    
-    chartGroup
-      .append("text")
-      .attr("x", width / 2)
-      .attr("y", height + 65)
-      .attr("text-anchor", "middle")
-      .style("font-family", "'Merriweather'")
-      .style("font-size", "16px")
-      .text("Time Elapsed (seconds)");
+  const xAxis = d3.axisBottom(xScale).tickSize(7);
+  const yAxis = d3.axisLeft(yScale).tickSize(7);
 
-    chartGroup
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("x", -height / 2)
-      .attr("y", -60)
-      .attr("text-anchor", "middle")
-      .style("font-family", "'Merriweather'")
-      .style("font-size", "16px")
-      .text("Variance in Blood Volume Pulse");
-    
-    svg
-      .append("text")
-      .attr("x", svgWidth / 1.9)
-      .attr("y", height - 250)
-      .attr("text-anchor", "middle")
-      .style("font-family", "'Merriweather'")
-      .style("font-size", "24px")
-      .style("font-weight", "bolder")
-      .text("How Does the Variance of BVP Differ When the Brain is at Rest");
-    
-      svg
-      .append("text")
-      .attr("x", svgWidth / 1.9)
-      .attr("y", height - 227)
-      .attr("text-anchor", "middle")
-      .style("font-family", "'Merriweather'")
-      .style("font-size", "24px")
-      .style("font-weight", "bolder")
-      .text("Compared to When the Brain is Under Cognitive Load?");
-    
-    const legend = svg.append("g").attr("transform", "translate(800, 90)");
+  chartGroup
+    .append("g")
+    .attr("transform", `translate(0, 300)`)
+    .call(xAxis)
+    .selectAll("text")
+    .style("font-family", "'Merriweather'")
+    .style("font-size", "15px");
 
-    const cogCircle = legend
-        .append("circle")
-        .attr("cx", -25)
-        .attr("cy", 36)
-        .attr("r", 6)
-        .attr("fill", "#FFA500")
-        .style("cursor", "pointer")
-        .on("click", function() {
-            cogPath.raise();
-            d3.select(this).attr("r", 9);
-            baselineCircle.attr("r", 6);
-        });
-  
-    legend
-        .append("text")
-        .attr("x", -10)
-        .attr("y", 40)
-        .text("Cognitive Load")
-        .style("font-family", "'Merriweather'")
-        .style("font-size", "13px");
-  
-    const legend2 = svg.append("g").attr("transform", "translate(800, 115)");
-  
-    const baselineCircle = legend2
-        .append("circle")
-        .attr("cx", -25)
-        .attr("cy", 33)
-        .attr("r", 6)
-        .attr("fill", "#808080")
-        .style("cursor", "pointer")
-        .on("click", function() {
-            baselinePath.raise();
-            d3.select(this).attr("r", 9);
-            cogCircle.attr("r", 6);
-        });
-  
-    legend2
-        .append("text")
-        .attr("x", -10)
-        .attr("y", 37)
-        .text("Baseline")
-        .style("font-family", "'Merriweather'")
-        .style("font-size", "13px");
-  
-    const legendTitle = svg
-        .append("g")
-        .attr("transform", "translate(800, 105)");
-  
-    legendTitle
-        .append("text")
-        .attr("x", -40)
-        .attr("y", 5)
-        .text("Cognitive State")
-        .style("font-weight", "bold")
-        .style("font-family", "'Merriweather'")
-        .style("font-size", "15px");
+  chartGroup
+    .append("g")
+    .call(yAxis)
+    .selectAll("text")
+    .style("font-family", "'Merriweather'")
+    .style("font-size", "15px");
 
-    // Tooltip and vertical line
-    const tooltip = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("visibility", "hidden")
-        .style("position", "absolute")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "1px")
-        .style("border-radius", "5px")
-        .style("padding", "10px");
+  chartGroup
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", height + 65)
+    .attr("text-anchor", "middle")
+    .style("font-family", "'Merriweather'")
+    .style("font-size", "16px")
+    .text("Time Elapsed (seconds)");
 
-    const verticalLine = chartGroup.append("line")
-        .attr("class", "vertical-line")
-        .attr("y1", 0)
-        .attr("y2", height + 25)
-        .attr("stroke", "black")
-        .attr("stroke-width", 1)
-        .attr("stroke-dasharray", "4")
-        .style("visibility", "hidden");
-    
-        chartGroup.append("rect")
-        .attr("width", width)
-        .attr("height", height + 25)
-        .style("fill", "none")
-        .style("pointer-events", "all")
-        .on("mousemove", function(event) {
-            const mouseX = d3.pointer(event, this)[0];
-            const x0 = xScale.invert(mouseX);
-            const i = d3.bisector(d => d.time_elapsed).left;
-            const closestBaselineIndex = i(baselineData, x0);
-            const closestCogIndex = i(cogData, x0);
+  chartGroup
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("y", -60)
+    .attr("text-anchor", "middle")
+    .style("font-family", "'Merriweather'")
+    .style("font-size", "16px")
+    .text("Variance in Blood Volume Pulse");
 
-            const closestBaseline = baselineData[closestBaselineIndex];
-            const closestCog = cogData[closestCogIndex];
+  svg
+    .append("text")
+    .attr("x", svgWidth / 1.9)
+    .attr("y", height - 250)
+    .attr("text-anchor", "middle")
+    .style("font-family", "'Merriweather'")
+    .style("font-size", "24px")
+    .style("font-weight", "bolder")
+    .text("How Does the Variance of BVP Differ When the Brain is at Rest");
 
-            const closestTime = closestBaseline.time_elapsed;
+  svg
+    .append("text")
+    .attr("x", svgWidth / 1.9)
+    .attr("y", height - 227)
+    .attr("text-anchor", "middle")
+    .style("font-family", "'Merriweather'")
+    .style("font-size", "24px")
+    .style("font-weight", "bolder")
+    .text("Compared to When the Brain is Under Cognitive Load?");
 
-            verticalLine
-                .attr("x1", xScale(closestTime))
-                .attr("x2", xScale(closestTime))
-                .style("visibility", "visible");
+  const legend = svg.append("g").attr("transform", "translate(800, 90)");
 
-            tooltip
-                .html(`Seconds Elapsed: ${closestTime}<br>Baseline Var: ${closestBaseline.bvp.toFixed(2)}<br>Cognitive Load Var: ${closestCog.bvp.toFixed(2)}`)
-                .style("left", `${event.pageX + 10}px`)
-                .style("top", `${event.pageY - 28}px`)
-                .style("visibility", "visible");
-        })
-        .on("mouseout", function() {
-            verticalLine.style("visibility", "hidden");
-            tooltip.style("visibility", "hidden");
-        });
+  const cogCircle = legend
+    .append("circle")
+    .attr("cx", -25)
+    .attr("cy", 36)
+    .attr("r", 6)
+    .attr("fill", "#FFA500")
+    .style("cursor", "pointer")
+    .on("click", function () {
+      cogPath.raise();
+      d3.select(this).attr("r", 9);
+      baselineCircle.attr("r", 6);
+    });
+
+  legend
+    .append("text")
+    .attr("x", -10)
+    .attr("y", 40)
+    .text("Cognitive Load")
+    .style("font-family", "'Merriweather'")
+    .style("font-size", "13px");
+
+  const legend2 = svg.append("g").attr("transform", "translate(800, 115)");
+
+  const baselineCircle = legend2
+    .append("circle")
+    .attr("cx", -25)
+    .attr("cy", 33)
+    .attr("r", 6)
+    .attr("fill", "#808080")
+    .style("cursor", "pointer")
+    .on("click", function () {
+      baselinePath.raise();
+      d3.select(this).attr("r", 9);
+      cogCircle.attr("r", 6);
+    });
+
+  legend2
+    .append("text")
+    .attr("x", -10)
+    .attr("y", 37)
+    .text("Baseline")
+    .style("font-family", "'Merriweather'")
+    .style("font-size", "13px");
+
+  const legendTitle = svg.append("g").attr("transform", "translate(800, 105)");
+
+  legendTitle
+    .append("text")
+    .attr("x", -40)
+    .attr("y", 5)
+    .text("Cognitive State")
+    .style("font-weight", "bold")
+    .style("font-family", "'Merriweather'")
+    .style("font-size", "15px");
+
+  // Tooltip and vertical line
+  const tooltip = d3
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("visibility", "hidden")
+    .style("position", "absolute")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px");
+
+  const verticalLine = chartGroup
+    .append("line")
+    .attr("class", "vertical-line")
+    .attr("y1", 0)
+    .attr("y2", height + 25)
+    .attr("stroke", "black")
+    .attr("stroke-width", 1)
+    .attr("stroke-dasharray", "4")
+    .style("visibility", "hidden");
+
+  chartGroup
+    .append("rect")
+    .attr("width", width)
+    .attr("height", height + 25)
+    .style("fill", "none")
+    .style("pointer-events", "all")
+    .on("mousemove", function (event) {
+      const mouseX = d3.pointer(event, this)[0];
+      const x0 = xScale.invert(mouseX);
+      const i = d3.bisector((d) => d.time_elapsed).left;
+      const closestBaselineIndex = i(baselineData, x0);
+      const closestCogIndex = i(cogData, x0);
+
+      const closestBaseline = baselineData[closestBaselineIndex];
+      const closestCog = cogData[closestCogIndex];
+
+      const closestTime = closestBaseline.time_elapsed;
+
+      verticalLine
+        .attr("x1", xScale(closestTime))
+        .attr("x2", xScale(closestTime))
+        .style("visibility", "visible");
+
+      tooltip
+        .html(
+          `Seconds Elapsed: ${closestTime}<br>Baseline Var: ${closestBaseline.bvp.toFixed(
+            2
+          )}<br>Cognitive Load Var: ${closestCog.bvp.toFixed(2)}`
+        )
+        .style("left", `${event.pageX + 10}px`)
+        .style("top", `${event.pageY - 28}px`)
+        .style("visibility", "visible");
+    })
+    .on("mouseout", function () {
+      verticalLine.style("visibility", "hidden");
+      tooltip.style("visibility", "hidden");
+    });
 }
 
 async function main() {
-    const dropdown = document.querySelector("#bvp-variance-grouping select");
+  const dropdown = document.querySelector("#bvp-variance-grouping select");
 
-    async function handleDropdownChange() {
-        const grouping = dropdown.value;
-        let file;
-        if (grouping === "half_second_var") {
-            file = "./data/bvp_variance_line_data/bvp_var_by_half_second.csv";
-        } else if (grouping === "tenth_second_var") {
-            file = "./data/bvp_variance_line_data/bvp_var_by_tenth_second.csv";
-        } else if (grouping === "second_var") {
-            file = "./data/bvp_variance_line_data/bvp_var_by_second.csv";
-        }
+  async function handleDropdownChange() {
+    const grouping = dropdown.value;
+    let file;
+    if (grouping === "half_second_var") {
+      file = "./data/bvp_variance_line_data/bvp_var_by_half_second.csv";
+    } else if (grouping === "tenth_second_var") {
+      file = "./data/bvp_variance_line_data/bvp_var_by_tenth_second.csv";
+    } else if (grouping === "second_var") {
+      file = "./data/bvp_variance_line_data/bvp_var_by_second.csv";
+    }
     const data = await loadData(file);
     updateChart(data);
-    }
+  }
 
-    dropdown.addEventListener("change", handleDropdownChange);
+  dropdown.addEventListener("change", handleDropdownChange);
 
-    // Initial chart rendering
-    await handleDropdownChange();
+  // Initial chart rendering
+  await handleDropdownChange();
 }
 
 main();
---
+++ parallel_plot.js
@@ -0,0 +1,230 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+
+let gamified_data = [];
+let non_gamified_data = [];
+
+async function loadData() {
+  gamified_data = await d3.csv(
+    "./data/original_gamified_stats_by_second.csv",
+    (d) => {
+      d.time_elapsed = parseFloat(d.time_elapsed);
+      d.bvp = parseFloat(d.bvp);
+      d.eda = parseFloat(d.eda);
+      d.temp = parseFloat(d.temp);
+      return d;
+    }
+  );
+
+  non_gamified_data = await d3.csv(
+    "./data/original_non_gamified_stats_by_second.csv",
+    (d) => {
+      d.time_elapsed = parseFloat(d.time_elapsed);
+      d.bvp = parseFloat(d.bvp);
+      d.eda = parseFloat(d.eda);
+      d.temp = parseFloat(d.temp);
+      return d;
+    }
+  );
+}
+
+const margin = { top: 30, right: 150, bottom: 30, left: 150 };
+const width = 800 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+const features = ["bvp", "eda", "temp"];
+
+let svg = d3
+  .select("#parallel-coordinate-plot")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+async function drawParallelCoordinatePlot() {
+  await loadData();
+
+  const xScale = d3.scalePoint().domain(features).range([0, width]);
+
+  const yScale = {};
+  features.forEach((feature) => {
+    const minVal = d3.min(
+      gamified_data.concat(non_gamified_data),
+      (d) => d[feature]
+    );
+    const maxVal = d3.max(
+      gamified_data.concat(non_gamified_data),
+      (d) => d[feature]
+    );
+    const padding = (maxVal - minVal) * 0.05;
+    yScale[feature] = d3
+      .scaleLinear()
+      .domain([minVal - padding, maxVal + padding])
+      .range([height, 0]);
+  });
+
+  // Create axes
+  const axes = svg
+    .selectAll(".axis")
+    .data(features)
+    .enter()
+    .append("g")
+    .attr("class", "axis")
+    .attr("transform", (d) => `translate(${xScale(d)})`)
+    .each(function (d) {
+      d3.select(this).call(d3.axisLeft(yScale[d]));
+    });
+
+  svg
+    .selectAll(".axis-label")
+    .data(features)
+    .enter()
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("x", (d) => xScale(d))
+    .attr("y", -10)
+    .attr("text-anchor", "middle")
+    .style("font-size", "14px")
+    .text((d) => d.toUpperCase());
+
+  svg
+    .selectAll(".gamified-line")
+    .data(gamified_data)
+    .enter()
+    .append("path")
+    .attr("class", "gamified-line")
+    .attr("d", (d) => lineGenerator(d, features, xScale, yScale))
+    .attr("fill", "none")
+    .attr("stroke", "darkorange")
+    .attr("stroke-width", 3)
+    .attr("opacity", 0.7);
+
+  svg
+    .selectAll(".non-gamified-line")
+    .data(non_gamified_data)
+    .enter()
+    .append("path")
+    .attr("class", "non-gamified-line")
+    .attr("d", (d) => lineGenerator(d, features, xScale, yScale))
+    .attr("fill", "none")
+    .attr("stroke", "gray")
+    .attr("stroke-width", 3)
+    .attr("opacity", 0.7);
+
+  const legend = svg
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", `translate(${width + 25}, 0)`);
+
+  legend
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", 20)
+    .attr("height", 20)
+    .attr("fill", "darkorange")
+    .attr("opacity", 0.8)
+    .attr("rx", 5)
+    .attr("ry", 5);
+
+  legend
+    .append("text")
+    .attr("x", 30)
+    .attr("y", 10)
+    .text("Gamified")
+    .style("font-size", "14px")
+    .attr("alignment-baseline", "middle");
+
+  legend
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", 30)
+    .attr("width", 20)
+    .attr("height", 20)
+    .attr("fill", "gray")
+    .attr("opacity", 0.8)
+    .attr("rx", 5)
+    .attr("ry", 5);
+
+  legend
+    .append("text")
+    .attr("x", 30)
+    .attr("y", 40)
+    .text("Non-Gamified")
+    .style("font-size", "14px")
+    .attr("alignment-baseline", "middle");
+
+  document.getElementById("slider").value = 1;
+  const value = document.querySelector("#sec-num");
+  const input = document.querySelector("#slider");
+  value.textContent = input.value;
+
+  const gamifiedDataPoint = getDataPointByTimeElapsed(1, gamified_data);
+  const nonGamifiedDataPoint = getDataPointByTimeElapsed(1, non_gamified_data);
+
+  updateLines(gamifiedDataPoint, nonGamifiedDataPoint);
+
+  input.addEventListener("input", (event) => {
+    const second = parseInt(event.target.value, 10);
+    value.textContent = second;
+    const gamifiedDataPoint = getDataPointByTimeElapsed(second, gamified_data);
+    const nonGamifiedDataPoint = getDataPointByTimeElapsed(
+      second,
+      non_gamified_data
+    );
+    updateLines(gamifiedDataPoint, nonGamifiedDataPoint);
+  });
+
+  function updateLines(gamifiedDataPoint, nonGamifiedDataPoint) {
+    const transition = d3.transition().duration(50);
+
+    // Update gamified line and markers
+    svg
+      .selectAll(".gamified-line")
+      .datum(gamifiedDataPoint)
+      .transition(transition)
+      .attr("d", (d) => lineGenerator(d, features, xScale, yScale));
+
+    svg
+      .selectAll(".gamified-marker")
+      .data(features)
+      .join("circle")
+      .attr("class", "gamified-marker")
+      .transition(transition)
+      .attr("cx", (d) => xScale(d))
+      .attr("cy", (d) => yScale[d](gamifiedDataPoint[d]))
+      .attr("r", 5)
+      .attr("fill", "darkorange");
+
+    // Update non-gamified line and markers
+    svg
+      .selectAll(".non-gamified-line")
+      .datum(nonGamifiedDataPoint)
+      .transition(transition)
+      .attr("d", (d) => lineGenerator(d, features, xScale, yScale));
+
+    svg
+      .selectAll(".non-gamified-marker")
+      .data(features)
+      .join("circle")
+      .attr("class", "non-gamified-marker")
+      .transition(transition)
+      .attr("cx", (d) => xScale(d))
+      .attr("cy", (d) => yScale[d](nonGamifiedDataPoint[d]))
+      .attr("r", 5)
+      .attr("fill", "gray");
+  }
+}
+
+function lineGenerator(dataPoint, features, xScale, yScale) {
+  return d3.line()(
+    features.map(function (feature) {
+      return [xScale(feature), yScale[feature](dataPoint[feature])];
+    })
+  );
+}
+
+function getDataPointByTimeElapsed(second, data) {
+  return data.filter((d) => d.time_elapsed === second)[0];
+}
+
+drawParallelCoordinatePlot();
